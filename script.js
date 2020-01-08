429dff7b04861bd0965119ee0dbb597798cbdd34
dc <chendana95@gmail.com>
2020-01-07 23:32:22 -0500

    created array for cities and added from input area

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-$("#search").on("click", function() {
-    //search from input value to create result buttons
+var cityName = "";
+var cityArray = [];
 
+//add to cityArray from input value
+$("#search").on("click", function() {
+    event.preventDefault();
+    var search = $("#enterCity").val().trim();
+    cityArray.push(search);
+    console.log(cityArray);
 });
 
 
 //selected city? ...grab value of selected button to search
 $("").on("click", function() {
-    var queryURL = "";
+
+    var apiKey = "af2a8b1f2b24299811ace630f9553342";
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
 
     $.ajax({
-        url: queryUrl,
+        url: queryURL,
         method: "GET"
     }).then(function(response) {
         console.log(response);
     });
 });
 
-//create a function to make an ajax call for UV Index
+
+//use moment.js to get date 
+//create a function to make an ajax call for UV Index`
+//function to grab from array of cities to create buttons in $("#result") area
